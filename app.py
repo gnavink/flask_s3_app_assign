c6556c942414e01bb6e1af2a4e9d63bc534de54c
gnavink <gnavink75@gmail.com>
2022-05-07 04:13:58 +0000

    added app.run

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from config import app,s3_app
 import routes
 
 if __name__=="__main__":
-    #app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0")
     s3_app.run(debug=True,host="0.0.0.0")
